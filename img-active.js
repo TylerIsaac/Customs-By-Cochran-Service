c8f157c761e2e399c1d7fa41ff5b79c3af54e84f
Tyler Isaac <82226147+TylerIsaac@users.noreply.github.com>
2022-05-23 12:14:28 -0400

    Add files via upload

---
+++ img-active.js
@@ -0,0 +1,71 @@
+function changeLift() {
+  var image = document.getElementById('service-img');
+
+  if (image.src.match("https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/LiftKit_Services.jpg")) {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/Default_Services.jpg";
+  }
+
+  else {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/LiftKit_Services.jpg";
+  }
+}
+
+function changeWheels() {
+  var image = document.getElementById('service-img');
+
+  if (image.src.match("https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/CustomsWebsite_Wheels_Services.jpg")) {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/Default_Services.jpg";
+  }
+
+  else {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/CustomsWebsite_Wheels_Services.jpg";
+  }
+}
+
+function changeTires() {
+  var image = document.getElementById('service-img');
+
+  if (image.src.match("https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/Tires_Services.jpg")) {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/Default_Services.jpg";
+  }
+
+  else {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/Tires_Services.jpg";
+  }
+}
+
+function changeRunningBoards() {
+  var image = document.getElementById('service-img');
+
+  if (image.src.match("https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/RunningBorad_Service.jpg")) {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/Default_Services.jpg";
+  }
+
+  else {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/RunningBorad_Service.jpg";
+  }
+}
+
+function changeTruckBedCovers() {
+  var image = document.getElementById('service-img');
+
+  if (image.src.match("https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/CustomsWebsite_TruckBedCovers_Services.jpg")) {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/Default_Services.jpg";
+  }
+
+  else {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/CustomsWebsite_TruckBedCovers_Services.jpg";
+  }
+}
+
+function changeHighPerformance() {
+  var image = document.getElementById('service-img');
+
+  if (image.src.match("https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/CustomsWebsite_Accessories_Services.jpg")) {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/Default_Services.jpg";
+  }
+
+  else {
+      image.src = "https://di-uploads-pod6.dealerinspire.com/1cochran/uploads/2022/05/CustomsWebsite_Accessories_Services.jpg";
+  }
+}
